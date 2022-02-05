b8153868cb34e81da166d2ff99aca9be74f29018
tiago <tiago@TIAGO>
2022-02-05 18:16:43 +0000

    Update form validations and fetch data from API

--- FormsApp/ClientApp/src/components/FetchData.js
+++ FormsApp/ClientApp/src/components/FetchData.js
@@ -1,59 +1,65 @@
 import React, { Component } from 'react';
 
 export class FetchData extends Component {
-  static displayName = FetchData.name;
+    static displayName = FetchData.name;
 
-  constructor(props) {
-    super(props);
-    this.state = { forecasts: [], loading: true };
-  }
+    constructor(props) {
+        super(props);
+        this.state = { forms: [], loading: true };
+    }
 
-  componentDidMount() {
-    this.populateWeatherData();
-  }
+    componentDidMount() {
+        this.populateFormsData();
+    }
 
-  static renderForecastsTable(forecasts) {
-    return (
-      <table className='table table-striped' aria-labelledby="tabelLabel">
-        <thead>
-          <tr>
-            <th>Date</th>
-            <th>Temp. (C)</th>
-            <th>Temp. (F)</th>
-            <th>Summary</th>
-          </tr>
-        </thead>
-        <tbody>
-          {forecasts.map(forecast =>
-            <tr key={forecast.date}>
-              <td>{forecast.date}</td>
-              <td>{forecast.temperatureC}</td>
-              <td>{forecast.temperatureF}</td>
-              <td>{forecast.summary}</td>
-            </tr>
-          )}
-        </tbody>
-      </table>
-    );
-  }
+    static renderFormsTable(forms) {
+        return (
+            <table className='table table-striped' aria-labelledby="tabelLabel">
+                <thead>
+                    <tr>
+                        <th>Data criação</th>
+                        <th>Nome</th>
+                        <th>Assunto</th>
+                        <th>Email</th>
+                        <th></th>
+                        <th></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {forms.map(form =>
+                        <tr key={form.Id}>
+                            <td>{form.CreationDate}</td>
+                            <td>{form.Name}</td>
+                            <td>{form.Subject}</td>
+                            <td>{form.Email}</td>
+                            <td><span onClick={ }>Marcar como visto</span></td>
+                            <td>Marcar como respondido</td>
+                        </tr>
+                    )}
+                </tbody>
+            </table>
+        );
+    }
 
-  render() {
-    let contents = this.state.loading
-      ? <p><em>Loading...</em></p>
-      : FetchData.renderForecastsTable(this.state.forecasts);
+    render() {
+        let contents = this.state.loading
+            ? <p><em>A carregar...</em></p>
+            : FetchData.renderFormsTable(this.state.forms);
 
-    return (
-      <div>
-        <h1 id="tabelLabel" >Weather forecast</h1>
-        <p>This component demonstrates fetching data from the server.</p>
-        {contents}
-      </div>
-    );
-  }
+        return (
+            <div>
+                <h1 id="tabelLabel" >Formularios enviados</h1>
+                <p>Aqui pode ver os formularios já enviados.</p>
+                {contents}
+            </div>
+        );
+    }
 
-  async populateWeatherData() {
-    const response = await fetch('weatherforecast');
-    const data = await response.json();
-    this.setState({ forecasts: data, loading: false });
-  }
+    async populateFormsData() {
+        const response = await fetch('/forms/get-forms');
+        const data = await response.json();
+        this.setState({ forms: JSON.parse(data.message), loading: false });
+
+        this.state.forms.map(form => console.log(form));
+    }
 }
--- FormsApp/ClientApp/src/components/Home.js
+++ FormsApp/ClientApp/src/components/Home.js
@@ -1,6 +1,6 @@
-import React, { Component, useState } from 'react';
+import React, { useState } from 'react';
 
-function App() {
+function Home() {
     const [values, setValues] = useState({
         name: "",
         subject: "",
@@ -9,40 +9,71 @@ function App() {
         message: ""
     });
 
+    const [errors, setErrors] = useState({
+        name: "",
+        subject: "",
+        contact: "",
+        email: "",
+        message: ""
+    });
+
+    let handleFocus = (name) => {
+        setErrors({ ...errors, [name]: '' })
+    }
+
     let handleSubmit = async (e) => {
         e.preventDefault();
-        try {
-            let res = await fetch("https://localhost:44479/forms/create", {
-                method: "POST",
-                headers: {
-                    'Accept': 'application/json, text/plain',
-                    'Content-Type': 'application/json;charset=UTF-8'
-                },
-                body: JSON.stringify({
-                    name: values.name,
-                    subject: values.subject,
-                    contact: values.contact,
-                    email: values.email,
-                    message: values.message,
-                }),
-            });
-            console.log(res);
-            let resJson = await res.json();
-            console.log(resJson);
-            if (res.status === 200) {
-                setValues({ ...values, name: "", email: "" })
-                console.log("User created successfully");
-            } else {
-                console.log("Some error occured");
+        let valid = true;
+
+        if (values.name === '') {
+            setErrors({ ...errors, name: "O nome é obrigatório" });
+            valid = false;
+        }
+
+        if (values.subject === '') {
+            setErrors({ ...errors, subject: "O assunto é obrigatório" });
+            valid = false;
+        }
+
+        if (values.contact === '') {
+            setErrors({ ...errors, contact: "O contacto é obrigatório" });
+            valid = false;
+        }
+
+        if (values.email === '') {
+            setErrors({ ...errors, email: "O email é obrigatório" });
+            valid = false;
+        }
+
+        if (valid) {
+            try {
+                let res = await fetch("https://localhost:44479/forms/create", {
+                    method: "POST",
+                    headers: {
+                        'Accept': 'application/json, text/plain',
+                        'Content-Type': 'application/json;charset=UTF-8'
+                    },
+                    body: JSON.stringify({
+                        name: values.name,
+                        subject: values.subject,
+                        contact: values.contact,
+                        email: values.email,
+                        message: values.message,
+                    }),
+                });
+                let resJson = await res.json();
+                console.log(resJson);
+                if (res.status === 200) {
+                    setValues({ ...values, name: "", email: "" })
+                } else {
+                }
+            } catch (err) {
+                console.log(err);
             }
-        } catch (err) {
-            console.log(err);
         }
     };
 
     let inputHadle = (name, value) => {
-        console.log(name);
-        console.log(value);
         setValues({ ...values, [name]: value })
     }
 
@@ -60,8 +91,10 @@ function App() {
                                 value={values.name}
                                 placeholder="Nome"
                                 name="name"
+                                required="required"
                                 onChange={(e) => inputHadle(e.target.name, e.target.value)}
                             />
+                            <span>{errors.name}</span>
                         </div>
                     </div>
                     <div className="col-12 col-md-6">
@@ -73,8 +106,10 @@ function App() {
                                 value={values.subject}
                                 placeholder="Assunto"
                                 name="subject"
+                                required="required"
                                 onChange={(e) => inputHadle(e.target.name, e.target.value)}
                             />
+                            <span>{errors.subject}</span>
                         </div>
                     </div>
                     <div className="col-12 col-md-6">
@@ -86,8 +121,10 @@ function App() {
                                 value={values.contact}
                                 placeholder="Contacto"
                                 name="contact"
+                                required="required"
                                 onChange={(e) => inputHadle(e.target.name, e.target.value)}
                             />
+                            <span>{errors.contact}</span>
                         </div>
                     </div>
                     <div className="col-12 col-md-6">
@@ -99,8 +136,10 @@ function App() {
                                 value={values.email}
                                 placeholder="Email"
                                 name="email"
+                                required="required"
                                 onChange={(e) => inputHadle(e.target.name, e.target.value)}
                             />
+                            <span>{errors.email}</span>
                         </div>
                     </div>
                     <div className="col-12">
@@ -125,159 +164,7 @@ function App() {
                 <button type="submit" className="btn btn-primary">Enviar</button>
             </form>
         </div>
-
-
-
-
-        //<div className="App">
-        //    <form onSubmit={handleSubmit}>
-        //        <input
-        //            type="text"
-        //            value={values.name}
-        //            placeholder="Nome"
-        //            onChange={(e) => inputHadle(e.target.name, e.target.value)}
-        //        />
-        //        <input
-        //            type="text"
-        //            value={values.email}
-        //            placeholder="Email"
-        //            onChange={(e) => inputHadle(e.target.name, e.target.value)}
-        //        />
-        //        <input
-        //            type="text"
-        //            value={values.contact}
-        //            placeholder="Mobile Number"
-        //            onChange={(e) => inputHadle(e.target.name, e.target.value)}
-        //        />
-        //        <button type="submit">Create</button>
-        //    </form>
-        //</div>
     );
 }
 
-export default App;
-
-//export default Home;
-
-//export class Home extends Component {
-//    static displayName = Home.name;
-
-//    constructor(props) {
-//        super(props);
-//        this.state = {
-//            name: "Ola",
-//            subject: "",
-//            contact: "",
-//            email: "",
-//            message: "",
-//            isValid: false,
-//        };
-//    }
-
-//    async onSubmit(event) {
-//        event.preventDefault();
-//        console.log(this.name);
-//        try {
-//            let res = await fetch("https://localhost:44479/forms/create", {
-//                method: "POST",
-//                body: JSON.stringify({
-//                    name: this.state.name,
-//                    subject: this.state.subject,
-//                    contact: this.state.contact,
-//                    email: this.state.email,
-//                    message: this.state.message,
-//                }),
-//            });
-//            console.log(res);
-//            let resJson = await res.json();
-//            if (res.status === 200) {
-//                this.setState({
-//                    name: "",
-//                    email: ""
-//                });
-//                console.log("User created successfully");
-//            } else {
-//                console.log("Some error occured");
-//            }
-//        } catch (err) {
-//            console.log(err);
-//        }
-//    }
-
-//    handleNameChange = (e) => {
-//        this.setState({
-//            name: e.target.value
-//        });
-//    }
-
-//    handleSubjectChange = (e) => {
-//        this.setState({
-//            subject: e.target.value
-//        });
-//    }
-
-//    handleContactChange = (e) => {
-//        this.setState({
-//            contact: e.target.value
-//        });
-//    }
-
-//    handleEmailChange = (e) => {
-//        this.setState({
-//            email: e.target.value
-//        });
-//    }
-
-//    handleMessageChange = (e) => {
-//        this.setState({
-//            message: e.target.value
-//        });
-//    }
-
-//    render() {
-//        return (
-//            <div className="form-container container">
-//                <form onSubmit={this.onSubmit}>
-//                    <div className="row">
-//                        <div className="col-12 col-md-6">
-//                            <div className="mb-3">
-//                                <label className="form-label">Nome</label>
-//                                <input type="text" className="form-control" name="name" onChange={this.handleNameChange} />
-//                            </div>
-//                        </div>
-//                        <div className="col-12 col-md-6">
-//                            <div className="mb-3">
-//                                <label className="form-label">Assunto</label>
-//                                <input type="text" className="form-control" name="subject" onChange={this.handleSubjectChange} />
-//                            </div>
-//                        </div>
-//                        <div className="col-12 col-md-6">
-//                            <div className="mb-3">
-//                                <label className="form-label">Contacto</label>
-//                                <input type="text" className="form-control" name="contact" onChange={this.handleContactChange} />
-//                            </div>
-//                        </div>
-//                        <div className="col-12 col-md-6">
-//                            <div className="mb-3">
-//                                <label className="form-label">Email</label>
-//                                <input type="email" className="form-control" name="email" onChange={this.handleEmailChange} />
-//                            </div>
-//                        </div>
-//                        <div className="col-12">
-//                            <div className="mb-3">
-//                                <textarea name="message" className="form-control" rows="3" onChange={this.handleMessageChange}></textarea>
-//                            </div>
-//                        </div>
-//                        <div className="col-12 col-md-6">
-//                            <div className="mb-3 form-check">
-//                                <input type="checkbox" className="form-check-input" />
-//                                <label className="form-check-label">Check me out</label>
-//                            </div>
-//                        </div>
-//                    </div>
-//                    <button type="submit" className="btn btn-primary">Enviar</button>
-//                </form>
-//            </div>
-//        );
-//    }
-//}
+export default Home;
